7f51722a5e1fdb351eff249ff01a1900e4149f59
Tatsiana Suyarkova <tankor@bk.ru>
2022-02-20 11:28:49 +0300

    Initial Commit

---
+++ TheFlashLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheFlashLight
+//
+//  Created by Дмитрий on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
